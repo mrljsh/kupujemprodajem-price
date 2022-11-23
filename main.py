af9b2de62c672c5ab052b4d6979d46a03f43e004
Mrljes <dragan.mrljes@vsar.edu.rs>
2022-11-23 21:16:39 +0100

    Python naming conventions

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import bs4
 import requests
 
-def getKPprice(productLink):
-    res = requests.get(productLink)
+
+def getKPprice(product_link):
+    res = requests.get(product_link)
     res.raise_for_status()
 
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    priceElement = soup.select('.AdViewInfo_price__PfKUL')
-    if priceElement:
-        priceList = [text.strip() for text in priceElement[0].text.split(" ")]
-        return priceList
+    price_element = soup.select('.AdViewInfo_price__PfKUL')
+    if price_element:
+        price_list = [text.strip() for text in price_element[0].text.split(" ")]
+        return price_list
     else:
         print("Greska, cena nije pronadjena. Proverite da li je link ispravan.")
         return
