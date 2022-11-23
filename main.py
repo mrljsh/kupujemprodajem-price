e9f20b612aec8019ba0f2e407ced94154f821a7c
Mrljes <dragan.mrljes@vsar.edu.rs>
2022-11-23 20:20:31 +0100

    Changes in getKPprice, returns price as list because of currency differences for future use

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ def getKPprice(productLink):
     res.raise_for_status()
 
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    priceElement = soup.select('.AdViewInfo_price__PfKUL');
-    return priceElement[0].text.strip()[6:]
+    priceElement = soup.select('.AdViewInfo_price__PfKUL')
+    priceList = [text.strip() for text in priceElement[0].text.split(" ")]
+    return priceList
 
-price = getKPprice('https://novi.kupujemprodajem.com/kompjuteri-laptop-i-tablet/laptopovi/thinkpad-t14-14-fhd-i5-10th-16gb-256nvme-garancija/oglas/143348543')
-print(price)
+price = getKPprice('https://novi.kupujemprodajem.com/casopisi-i-stripovi/stripovi-alan-ford/alan-ford/oglas/144373031')
+print('Cena je: ' + ' '.join(price[1:3]))
